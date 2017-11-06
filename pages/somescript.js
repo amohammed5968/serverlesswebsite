c31743499e03b58e3aa4a38f451c3d4a432e0b5e
amohammed5968 <mohd.ahsannaveed@gmail.com>
2017-11-06 10:58:53 -0800

    Update somescript.js

--- pages/somescript.js
+++ pages/somescript.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
         $('.parallax').parallax();
 
-    $("#btn1").click(function(){
-        $("#test1").text("Hello world!");
-    });
-    $("#btn2").click(function(){
-        $("#test2").html("<b>Hello world!</b>");
-    });
-    $("#btn3").click(function(){
-        $("#test3").val("Dolly Duck");
-    });
 });
